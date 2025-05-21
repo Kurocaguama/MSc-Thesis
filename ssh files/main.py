27a2d7f83dcaf070c3a7e63ae7d58610d5de9045
Francisco <francisco.lopez.ponce@ciencias.unam.mx>
2025-05-21 15:41:56 -0600

    ola dios

--- ssh files/main.py
+++ ssh files/main.py
@@ -7,12 +7,13 @@ hf_key = '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'
 huggingface_hub.login(hf_key)
 
 model_id = 'meta-llama/Llama-3.2-1B-Instruct'
+dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 generation_config = GenerationConfig.from_pretrained(model_id)
 
 # Declaración de modelo y tokenizador
 tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForCausalLM.from_pretrained(model_id, cache_dir = '/media/discoexterno/francisco/modelos', generation_config = generation_config)
+model = AutoModelForCausalLM.from_pretrained(model_id, cache_dir = '/media/discoexterno/francisco/modelos', generation_config = generation_config).to(dev)
 model.generation_config.pad_token_id = tokenizer.pad_token_id
 tokenizer.pad_token = tokenizer.eos_token
 
