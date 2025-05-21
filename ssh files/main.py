b4bd09a2197c9920a1029533faf2ff228b3c8d25
Francisco <francisco.lopez.ponce@ciencias.unam.mx>
2025-05-21 16:33:46 -0600

    XD me lleva

--- ssh files/main.py
+++ ssh files/main.py
@@ -1,7 +1,7 @@
 import torch, trl, huggingface_hub
 from datasets import load_dataset
 from trl import DPOConfig, DPOTrainer
-from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig
+from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig, BitsAndBytesConfig
 
 hf_key = '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'
 huggingface_hub.login(hf_key)
@@ -10,11 +10,12 @@ model_id = 'meta-llama/Llama-2-7b-hf'
 dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 torch.cuda.empty_cache()
 
+quant_config = BitsAndBytesConfig(load_in_4bit = True, bnb_4bit_compute_dtype = torch.bfloat16)
 generation_config = GenerationConfig.from_pretrained(model_id)
 
 # Declaración de modelo y tokenizador
 tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForCausalLM.from_pretrained(model_id, cache_dir = '/media/discoexterno/francisco/modelos', generation_config = generation_config).to(dev)
+model = AutoModelForCausalLM.from_pretrained(model_id, cache_dir = '/media/discoexterno/francisco/modelos', quantization_config = quant_config, generation_config = generation_config).to(dev)
 model.generation_config.pad_token_id = tokenizer.pad_token_id
 tokenizer.pad_token = tokenizer.eos_token
 
--- ssh files/tests.py
+++ ssh files/tests.py
@@ -3,5 +3,5 @@ print(torch.cuda.device_count())
 for i in range(torch.cuda.device_count()):
     print(torch.cuda.get_device_properties(i).name)
     
-dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+dev = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
 print(dev)
