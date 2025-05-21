a2532f856d06c4ccc8aafc848df7d54485ab90ff
Francisco <francisco.lopez.ponce@ciencias.unam.mx>
2025-05-21 15:39:28 -0600

    Update tests.py

--- ssh files/tests.py
+++ ssh files/tests.py
@@ -1,3 +1,6 @@
 import torch
 for i in range(torch.cuda.device_count()):
     print(torch.cuda.get_device_properties(i).name)
+    
+dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(dev)
